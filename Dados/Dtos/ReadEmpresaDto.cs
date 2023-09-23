9acad519f84f6412ff4d1c52a858062e7283157e
Daniela-Neves <rm86381@fiap.com.br>
2023-09-22 23:55:00 -0300

    adicionando id no login de empresa

--- Dados/Dtos/ReadEmpresaDto.cs
+++ Dados/Dtos/ReadEmpresaDto.cs
@@ -2,6 +2,7 @@
 {
     public class ReadEmpresaDto
     {
+        public int Id { get; set; }
         public string Cnpj { get; set; }
         public string RazaoSocial { get; set; }
         public ReadEnderecoDto Endereco { get; set; }
